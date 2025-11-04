4ec7c0ec60ca494ffd6bcbb4115584f0fdf956ea
Sebasx032 <sebastx032@gmail.com>
2025-11-04 11:59:59 -0500

    Create codigoModificado.c

---
+++ Ejercicio3.1.3/codigoModificado.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
+
+int pid;
+
+int main() {
+    pid = fork();
+
+    switch (pid) {
+        case -1:
+            printf("\nNo he podido crear el proceso hijo");
+            break;
+
+        case 0:
+            printf("\nSoy el hijo, mi PID es %d y mi PPID es %d", getpid(), getppid());
+            break;
+
+        default:
+            printf("\nEsperando al hijo");
+            wait(0); // Espera a que termine el hijo
+            printf("\nSoy el padre, mi PID es %d y el PID de mi hijo es %d", getpid(), pid);
+    }
+
+    printf("\nFinal de ejecución de %d \n", getpid());
+    exit(0);
+}
