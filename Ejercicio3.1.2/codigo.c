6d6a743ecd96b3246f9b7ddcaa6d49c1d359eda2
Sebasx032 <sebastx032@gmail.com>
2025-11-04 15:12:43 -0500

    Update codigo.c

--- Ejercicio3.1.2/codigo.c
+++ Ejercicio3.1.2/codigo.c
@@ -1,1 +1,31 @@
-codigo xD
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/wait.h>
+int main()
+{
+ int pid;
+ int i;
+ pid = fork();
+ switch(pid)
+ {
+ case -1: // Si pid es -1 quiere decir que ha habido un error
+ printf("No se ha podido crear el proceso hijo\n");
+ break;
+ case 0: // Cuando pid es cero quiere decir que es el proceso hijo
+ for(i=1; i<=10; i++)
+ printf("%d Soy el hijo con PID: %d \n",i,getpid());
+ break;
+ default: // Cuando es distinto de cero es el padre
+ for(i=1; i<=10; i++)
+ printf("%d Soy el padre con PID: %d \n",i,getpid());
+ // La función wait detiene el proceso padre y se queda esperando hasta
+ // que termine el hijo
+ wait(0);
+ break;
+ }
+}
+3.1.2 Modifique el código de la Figura 2 para que el proceso padre
+incremente 10 veces el valor de una variable en pasos de 10,
+mientras que el proceso hijo registre cada valor en un archivo
+de texto.
+
